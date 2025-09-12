b08bc82ceba81ceba743bcc4815dde22f51df268
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-09-12 01:02:42 -0300

    refactor: remove unused import of PasswordsDoNotMatchError

--- src/use-cases/users/register.ts
+++ src/use-cases/users/register.ts
@@ -1,7 +1,6 @@
 import type { UsersRepository } from '@/repositories/users-repository.ts'
 import { hash } from 'bcryptjs'
 import type { User } from 'generated/prisma/index.js'
-import { PasswordsDoNotMatchError } from '../errors/passwords-do-not-match.ts'
 import { UserAlreadyExistsError } from '../errors/user-already-exists-error.ts'
 
 interface RegisterUseCaseRequest {
