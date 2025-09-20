5835fa3e847bdc06601fc19c07dde84125ac3a99
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-09-19 21:12:22 -0300

    test: remove unnecessary date configurations and simplify test setup

--- src/http/controllers/tasks/create.spec.ts
+++ src/http/controllers/tasks/create.spec.ts
@@ -29,17 +29,8 @@ describe('Create Task (e2e)', () => {
   })
 
   it('should be able to create a task', async () => {
-    const pastDate = new Date()
-    pastDate.setMonth(pastDate.getMonth() - 1)
-    pastDate.setHours(12, 0, 0, 0)
-
     const futureDate = new Date()
     futureDate.setMonth(futureDate.getMonth() + 1)
-    futureDate.setHours(12, 0, 0, 0)
-
-    const farFutureDate = new Date()
-    farFutureDate.setMonth(farFutureDate.getMonth() + 6)
-    farFutureDate.setHours(12, 0, 0, 0)
 
     const user = await prisma.user.findFirstOrThrow()
 
