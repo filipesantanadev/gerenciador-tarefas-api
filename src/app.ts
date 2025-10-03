763a5928d5604924ae2726b33774cb8a51ccfa80
filipesantanadev <filipe.santana@al.infnet.edu.br>
2025-10-02 22:38:24 -0300

    feat: implement refresh token functionality with cookie support and e2e tests

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { usersRoutes } from './http/controllers/users/routes.ts'
 import { ZodError } from 'zod'
 import { env } from './env/index.ts'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { tasksRoutes } from './http/controllers/tasks/routes.ts'
 import { categoriesRoutes } from './http/controllers/categories/routes.ts'
 import { tagsRoutes } from './http/controllers/tags/routes.ts'
@@ -12,7 +13,16 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(tasksRoutes)
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,10 +4,13 @@ import { register } from './register.ts'
 import { authenticate } from './authenticate.ts'
 import { profile } from './profile.ts'
 import { update } from './update.ts'
+import { refresh } from './refresh.ts'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /* Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
